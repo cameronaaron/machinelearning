e1a8fc41768ee0ebf1fd45d056f089475398ecf3
Cameron Aaron <cameronaaron@users.noreply.github.com>
2020-03-04 11:07:42 -0500

    fin

--- Project2/main.py
+++ Project2/main.py
@@ -1,11 +1,52 @@
-import pandas as pd
-import os
-import numpy as np
+#!/usr/bin/env python3
 
-# open public data spreadsheet
-#Exondata = pd.read_csv("publicspreadsheet.csv")
-crimestatsdata = pd.read_csv("2018crimestatistics.csv")
-# Test print Exon
-print("raw data exon", Exondata)
-# crimestatsdata test print
-# print("raw data crimestatsdata", crimestatsdata)
+import pandas as pd
+#Import file
+Exondata = pd.read_csv("publicspreadsheet.csv")
+# Extract data
+race = Exondata['Race'].value_counts()
+sex = Exondata['Sex'].value_counts()
+state = Exondata['State'].value_counts()
+racetotal = 0
+#Find total race
+for x in race:
+    racetotal = race + x
+
+print(racetotal)
+
+probrace = race / racetotal
+# Find total sex
+sextotal = 0
+
+for x in sex:
+    sextotal = sex + x
+
+print(sextotal)
+probsex = sex / sextotal
+print(sex / sextotal)
+#Find total state
+statetotal = 0
+for x in state:
+    statetotal = state + x
+
+print(statetotal)
+
+probstate = state / statetotal
+
+prob = 1
+x = 0
+y = 0
+z = 0
+
+for a in probrace:
+    print("the race" + Exondata.Race[x])
+    x = x + 1
+    for b in probstate:
+        print("state", Exondata.State[y])
+        y = y + 1
+        for c in probsex:
+            print("sex", Exondata.Sex[z])
+            z = z = 1
+
+            probofcase = a * b * c
+            print(probofcase)
